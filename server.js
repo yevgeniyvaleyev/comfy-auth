8850f8c9239949478cdd7c4653c854c2f5b07406
Yevgeniy Valeyev <yevgeniy.valeyev@pwc.com>
2021-10-11 16:37:02 +0200

    chore(deploy): deployed to heroku

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 
+/**
+ * This server is only  needed for demo deploy.
+ */
 const app = express();
 
 app.use(express.static(__dirname + '/dist/comfy-auth'));
