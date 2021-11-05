3ec8e4059f33297819d453002ca7f6aa1d4fe278
Yevgeniy Valeyev <yevgeniy.valeyev@pwc.com>
2021-11-05 16:33:21 +0100

    chore(modules): finished migration

--- src/app/core/config/tokens.ts
+++
@@ -1,5 +0,0 @@
-
-import { AppConfig } from '../../types/app-config';
-import { InjectionToken } from '@angular/core';
-
-export const APP_CONFIG = new InjectionToken<AppConfig>('app.config');
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MainNavigationComponent } from './main-navigation.component';
 import { SharedModule } from '../shared/shared.module';
-import { APP_CONFIG } from './config/tokens';
-import { APP_CONFIG_DATA } from './config/config';
+import { APP_CONFIG } from './config';
+import { APP_CONFIG_DATA } from './config';
 
 @NgModule({
   declarations: [MainNavigationComponent],
